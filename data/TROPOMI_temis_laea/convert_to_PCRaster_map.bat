ece9d68fed5e2d2c91dbf9a8006ed761704ac9d6
co822ee <61116785+co822ee@users.noreply.github.com>
2020-02-17 18:54:10 +0100

    update bat files

--- data/TROPOMI_temis_laea/convert_to_PCRaster_map.bat
+++ data/TROPOMI_temis_laea/convert_to_PCRaster_map.bat
@@ -1,8 +1,5 @@
 @echo off
+
 for %%i in (*.grd) do gdal_translate -ot Float32 -of PCRaster -mo PCRASTER_VALUESCALE=VS_SCALAR %%i %%i.map
 
-REM get-childitem *.map | foreach {rename-item $_ $_.name.replace(".grd","")}
-REM get-childitem *.xml | foreach {rename-item $_ $_.name.replace(".grd","")}
-powershell.exe -EncodedCommand ZwBlAHQALQBjAGgAaQBsAGQAaQB0AGUAbQAgACoALgBtAGEAcAAgAHwAIABmAG8AcgBlAGEAYwBoACAAewByAGUAbgBhAG0AZQAtAGkAdABlAG0AIAAkAF8AIAAkAF8ALgBuAGEAbQBlAC4AcgBlAHAAbABhAGMAZQAoACIALgBnAHIAZAAiACwAIgAiACkAfQAKAGcAZQB0AC0AYwBoAGkAbABkAGkAdABlAG0AIAAqAC4AeABtAGwAIAB8ACAAZgBvAHIAZQBhAGMAaAAgAHsAcgBlAG4AYQBtAGUALQBpAHQAZQBtACAAJABfACAAJABfAC4AbgBhAG0AZQAuAHIAZQBwAGwAYQBjAGUAKAAiAC4AZwByAGQAIgAsACIAIgApAH0A 
-pause
 
